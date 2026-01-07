b2c5f1469342e7ba840409d89209c5058bf5628a
michel <michelrabahi1989@gmail.com>
2026-01-07 19:59:51 +0000

    first test

---
+++ src/main/kotlin/com/rabahi/grocery/Grocery.kt
@@ -0,0 +1,5 @@
+package com.rabahi.grocery
+
+class Grocery {
+    fun pricing():Double = 3.15
+}
---
+++ src/test/kotlin/com/rabahi/grocery/GroceryTest.kt
@@ -0,0 +1,16 @@
+package com.rabahi.grocery
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+
+class GroceryTest: BehaviorSpec({
+    val sut = Grocery()
+    given("i want to price up my basket") {
+        `when`("i have 3 tins of soup and 2 loaves of bread, bought today,") {
+            val pricing = sut.pricing()
+            then(" total cost = 3.15") {
+               pricing shouldBe 3.15
+            }
+        }
+    }
+})
